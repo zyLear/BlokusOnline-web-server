2e1ece282905ad57d142156831a9ae5062425d6b
xiezongyu <zongyu.xie@magnumwm.com>
2020-11-11 16:30:07 +0800

    add log file

--- blokus-server/blokus-websocket-server/src/main/java/com/zylear/blokus/wsserver/BlokusWebSocketServerApplication.java
+++ blokus-server/blokus-websocket-server/src/main/java/com/zylear/blokus/wsserver/BlokusWebSocketServerApplication.java
@@ -1,41 +1,16 @@
 package com.zylear.blokus.wsserver;
 
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.web.context.WebServerApplicationContext;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-import java.util.Properties;
-
-@SpringBootApplication
-//@EnableAutoConfiguration
-@ComponentScan("com.zylear.blokus.wsserver.*")
-@EnableAutoConfiguration(exclude = {
-////		DataSourceAutoConfiguration.class,
-//		DataSourceTransactionManagerAutoConfiguration.class,
-//		MybatisAutoConfiguration.class,
-////		DispatcherServletAutoConfiguration.class, /**如果需要自定义servlet dispatch,需要exclude*/
-////		ErrorMvcAutoConfiguration.class,
-////		RabbitAutoConfiguration.class,
-		DataSourceAutoConfiguration.class
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
 })
 public class BlokusWebSocketServerApplication {
 
-	public static void main(String[] args) {
-		Properties properties = System.getProperties();
-		System.out.println(properties);
-		SpringApplication.run(BlokusWebSocketServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlokusWebSocketServerApplication.class, args);
+    }
 
-//	@Override
-//	public void customize(ConfigurableEmbeddedServletContainer container) {
-//		container.setPort(8152);
-////		container.setContextPath("/blokus");
-//	}
 }
